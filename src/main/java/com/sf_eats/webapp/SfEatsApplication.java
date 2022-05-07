802bff6e77e82dbf42ab49424695d105de27fbe8
Carolina Lunardi Correa <88406130+Arete-Carolina@users.noreply.github.com>
2022-05-07 12:34:47 -0700

    Connected mySQL

--- src/main/java/com/sf_eats/webapp/SfEatsApplication.java
+++ src/main/java/com/sf_eats/webapp/SfEatsApplication.java
@@ -3,10 +3,12 @@ package com.sf_eats.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.sql.DataSource;
 
-@SpringBootApplication (exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
+@EnableJpaRepositories("com.sf_eats.webapp.repository")
 public class SfEatsApplication {
 
 	public static void main(String[] args) {

001bfad17e4c63434778fc40d979f5f55971d95f
Carolina Lunardi Correa <88406130+Arete-Carolina@users.noreply.github.com>
2022-05-07 23:39:50 -0700

    Refactored code.

--- src/main/java/com/sf_eats/webapp/controller/RestaurantController.java
+++ src/main/java/com/sf_eats/webapp/controller/RestaurantController.java
@@ -3,6 +3,8 @@ package com.sf_eats.webapp.controller;
 import com.sf_eats.webapp.model.Restaurant;
 import com.sf_eats.webapp.repository.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,12 +19,11 @@ public class RestaurantController {
     RestaurantRepository restaurantRepository;
 
     @GetMapping("/restaurants")
-    public ResponseEntity<List<Restaurant>> getAllRestaurants(@RequestParam(required = false) String sortBy) {
+    public ResponseEntity<Iterable<Restaurant>> getAllRestaurants(@RequestParam(required = false) String sortBy) {
         System.out.println(sortBy);
 
         try {
-            System.out.println(restaurantRepository.findAll());
-            List<Restaurant> restaurants = new ArrayList<Restaurant>(restaurantRepository.findAll());
+            Iterable<Restaurant> restaurants = restaurantRepository.findAll(PageRequest.of(0, 25, Sort.by(Sort.Direction.DESC,"id")));
 
             System.out.println(restaurants);
 
@@ -31,15 +32,15 @@ public class RestaurantController {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-//    @PostMapping("/restaurants")
-//    public ResponseEntity<Restaurant> createTutorial(@RequestBody Restaurant restaurant) {
-//        try {
-//            Restaurant _restaurant = restaurantRepository
-//                    .save(new Restaurant(restaurant.getTitle(), restaurant.getDescription(), false));
-//            return new ResponseEntity<>(_restaurant, HttpStatus.CREATED);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
+@PostMapping("/restaurants")
+    public ResponseEntity<Restaurant> createTutorial(@RequestBody Restaurant restaurant) {
+        try {
+            Restaurant _restaurant = restaurantRepository
+                    .save(new Restaurant(restaurant.getTitle(), restaurant.getDescription(), false));
+            return new ResponseEntity<>(_restaurant, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
 
--- src/main/java/com/sf_eats/webapp/repository/RestaurantRepository.java
+++ src/main/java/com/sf_eats/webapp/repository/RestaurantRepository.java
@@ -2,7 +2,8 @@ package com.sf_eats.webapp.repository;
 
 import com.sf_eats.webapp.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+public interface RestaurantRepository extends PagingAndSortingRepository<Restaurant, Long> {
 }

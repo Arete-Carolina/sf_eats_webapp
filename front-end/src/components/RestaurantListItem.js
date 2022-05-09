37ddee28688783c707dc33a148d67388a779d41a
Carolina Lunardi Correa <88406130+Arete-Carolina@users.noreply.github.com>
2022-05-09 02:52:38 -0700

    RestaurantDetail done

--- front-end/src/components/RestaurantListItem.js
+++ front-end/src/components/RestaurantListItem.js
@@ -1,9 +1,10 @@
 import { Nav } from 'react-bootstrap';
+import { Switch, Route, Link } from 'react-router-dom';
 
 export default function RenderingBasedOnFetchStatus({ id, applicant }) {
   return (
-    <Nav.Item as='li' key={id}>
-      <Nav.Link href={`/${id}`}>
+    <Nav.Item as='li' key={id} href={`/${id}`}>
+      <Nav.Link as={Link} to={`/${id}`}>
         {id}. {applicant}
       </Nav.Link>
     </Nav.Item>

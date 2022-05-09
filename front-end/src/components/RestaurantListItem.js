7bb7c673d10472fb7b92fca614b2b334c190d641
Carolina Lunardi Correa <88406130+Arete-Carolina@users.noreply.github.com>
2022-05-09 00:45:06 -0700

    pagination done

--- front-end/src/components/RestaurantListItem.js
+++ front-end/src/components/RestaurantListItem.js
@@ -1,3 +1,11 @@
-export default function RenderingBasedOnFetchStatus({ applicant }) {
-  return <li>{applicant}</li>;
+import { Nav } from 'react-bootstrap';
+
+export default function RenderingBasedOnFetchStatus({ id, applicant }) {
+  return (
+    <Nav.Item as='li' key={id}>
+      <Nav.Link href={`/${id}`}>
+        {id}. {applicant}
+      </Nav.Link>
+    </Nav.Item>
+  );
 }

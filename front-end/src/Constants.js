0c33fa24f5ed7405bd38481cbec4b4395da13e0d
Carolina Lunardi Correa <88406130+Arete-Carolina@users.noreply.github.com>
2022-05-09 16:12:08 -0700

    redirect backend call

--- front-end/src/Constants.js
+++ front-end/src/Constants.js
@@ -1,4 +1,6 @@
-export const BACKEND_URL = 'http://sfeatsbackend-env.eba-puurspjt.us-east-1.elasticbeanstalk.com/api/restaurants';
+// export const BACKEND_URL = 'http://sfeatsbackend-env.eba-puurspjt.us-east-1.elasticbeanstalk.com/api/restaurants';
+export const BACKEND_URL = '/backend';
+
 export class FetchStatus {
   static Success = new FetchStatus('Success');
   static Loading = new FetchStatus('Loading');
